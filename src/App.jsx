7ea84ba97837db760dd28e0f636f7743c18126b1
Deepaksharma084 <dk3433045@gmail.com>
2024-10-09 21:03:18 +0530

    Updated again

--- src/App.jsx
+++ src/App.jsx
@@ -97,25 +97,25 @@ function App() {
 
 
       <div className='text-white flex justify-center bg-slate-300 min-h-[70vh] '>
-        <div className='bg-[#85899f]  min-h-[70vh] w-[80vw] overflow-hidden rounded-xl my-4 max-sm:w-[90vw] max-sm:min-h-[170.9vw] max-sm:my-6 min-md:bg-red-900'>
+        <div className='bg-[#85899f]  min-h-[70vh] w-[80vw] overflow-hidden rounded-xl my-4 max-sm:w-[95vw] max-sm:min-h-[170.9vw] max-sm:my-6 min-md:bg-red-900'>
 
           <div>
             <h1 className='text-xl my-4 mx-10 text-black font-bold select-none'>Your task manager</h1>
           </div>
 
-          <div className='todo max-sm:bg-[#58565b] flex items-center justify-center gap-[2vw] h-[5vw] w-[78vw] max-sm:h-[14vw] max-sm:w-[86.9vw] mx-auto rounded-xl max-sm:rounded-xl max-sm:mx-auto'>
+          <div className='todo max-sm:bg-[#58565b] flex items-center justify-center gap-[2vw] h-[5vw] w-[78vw] max-sm:h-[14vw] max-sm:w-[89.9vw] mx-auto rounded-xl max-sm:rounded-xl max-sm:mx-auto'>
             <input onKeyDown={handleKeyDown} type="text" onChange={handlechange} value={todo} className='text-black hover:bg-[#eaebed] h-[2.1vw] w-[60vw]  rounded-xl p-2 max-sm:h-[8vw]' />
             <button onClick={handleSave} disabled={todo.length < 1} className='text-black flex justify-center items-center h-[2.1vw] w-[5vw] bg-[#29ff77] rounded-xl font-medium text-lg  max-sm:h-[8vw] max-sm:w-[15vw] select-none'>Save</button>{/* gets disabled if characters are less than 1 i.e 0*/}
           </div>
 
           <div className='flex items-center gap-3 w-[80vw]'>
-            <input onChange={toggleShowFinished} checked={ShowFinished} className='h-[0.9vw] ml-24 max-sm:h-[5vw] max-sm:ml-10' type="checkbox" name="" id="" />
+            <input onChange={toggleShowFinished} checked={ShowFinished} className='h-[0.9vw] ml-24 max-sm:h-[5vw] max-sm:ml-5 max-sm:my-5' type="checkbox" name="" id="" />
             <p className="text-[1vw] cursor-pointer text-black max-sm:text-[4vw] select-none">Show finished</p>
           </div>
 
           <hr className="h-[0.2vw] bg-gray-400" />
 
-          <h1 className='text-lg mx-10 text-black font-bold select-none'>Your Todos</h1>
+          <h1 className='text-lg mx-10 max-sm:my-5 text-black font-bold select-none'>Your Todos</h1>
 
 
           {todos.length === 0 &&
@@ -127,9 +127,11 @@ function App() {
           {todos.map(items => {
             return (ShowFinished || !items.isCompleted) && <div key={items.id} className='flex w-full my-4'>
 
-              <div className='flex gap-4 w-[60vw] break-words'>
+              <div className='mr-3 mt-1'>
+                <input name={items.id} onChange={toggleIsCompleted} checked={items.isCompleted} className='ml-24 max-sm:ml-5' type="checkbox" id="toggle" />
+              </div>
 
-                <input name={items.id} onChange={toggleIsCompleted} checked={items.isCompleted} className='ml-24 max-sm:ml-10' type="checkbox" id="toggle" />
+              <div className='flex gap-4 w-[60vw] break-words'>
                 <div className={items.isCompleted ? "line-through decoration-[#000]" : ""}>
                   <div className='text-black break-words w-[40vw] h-auto'>
                     {items.todo}
@@ -138,10 +140,14 @@ function App() {
 
               </div>
 
-              <div className='flex h-full gap-4'>
-                <button onClick={() => handleEdit(items.id)} className='ml-4 text-black flex justify-center items-center h-[1.7vw] max-sm:w-[5vw] bg-[#edffea] rounded-xl font-medium max-sm:text-lg max-sm:h-[6vw] w-[7vw] max-sm:rounded-md text-sm'><i class="ri-quill-pen-line"></i></button>
-                <button onClick={() => handleDelete(items.id)} className='text-black flex justify-center items-center h-[1.7vw] w-[5vw] bg-[#edffea] rounded-xl font-medium text-lg max-sm:h-[6vw] max-sm:w-[7vw] max-sm:rounded-md max-sm:text-sm'><i class="ri-delete-bin-6-line"></i></button>
+              <div className='flex h-full gap-3'>
+                
+                <button onClick={() => handleEdit(items.id)} className='ml-1 text-black flex justify-center items-center h-[1.7vw] max-sm:w-[7vw] bg-[#edffea] rounded-xl font-medium max-sm:text-lg max-sm:h-[6vw] w-[3vw] max-sm:rounded-md text-sm'><i class="ri-quill-pen-line"></i></button>
+               
+                <button onClick={() => handleDelete(items.id)} className='text-black flex justify-center items-center h-[1.7vw] w-[3vw] bg-[#edffea] rounded-xl font-medium text-lg max-sm:h-[6vw] max-sm:w-[7vw] max-sm:rounded-md max-sm:text-sm'><i class="ri-delete-bin-6-line"></i></button>
+
               </div>
+
 
             </div>
           })}
