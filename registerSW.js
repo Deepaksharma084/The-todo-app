39236b1b5cdde57e02b9e7468f628843e7bf7214
Deepaksharma084 <Deepaksharma084@users.noreply.github.com>
2024-10-13 18:21:38 +0000

    deploy: 96cf148a61c9cc275de02b01dd6c98e3da1b84cb

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/The-todo-app/sw.js', { scope: '/The-todo-app/' })})}
